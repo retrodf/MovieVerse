d1abef880a621a278dc09570d19656dfd259ab71
retrodf <114569570+retrodf@users.noreply.github.com>
2024-12-04 23:45:03 +0700

    konfigurasi vercel part 11

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Menentukan bahwa output folder adalah 'dist'
+  },
   test: {
     globals: true,
     environment: "jsdom",
